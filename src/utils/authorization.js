4e8d8334756a0e17b62d466fbe2e48b8f2cbc5ec
Alison Qiu <90943803+alisonqiu@users.noreply.github.com>
2022-05-27 19:28:38 -0500

    undo deleting authorization.js

---
+++ src/utils/authorization.js
@@ -0,0 +1,11 @@
+
+/*You need to change to your own token to get access to the database */
+/* I have not tried this. If bug, go to src\services\index.js\gen to do some adaption */
+export default {
+    authorization: {
+        "Authorization": 'Token 3e9ed2e0fa70a1a5cb6f34eb7a30ebde208ecd8f',
+        //"Content-Type": "multipart/form-data"
+        "Content-Type": "application/json"
+    }
+}
+
